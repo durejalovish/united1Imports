779c740a7e23f3c474dcaa883ff329cddf94cfda
lovish dureja <lovihdureja0786@gmail.com>
2024-01-27 01:30:42 -0500

    updated node

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-app.use(express.static('./dist/ngrx-app'));
+app.use(express.static('./dist/angular-tour-of-heroes'));
 app.get('/*', function(req, res){
-    res.sendFile('index.html', {root:'dist/ngrx-app'})
+    res.sendFile('index.html', {root:'dist/angular-tour-of-heroes'})
 });
 app.listen(process.env.PORT || 8080);
