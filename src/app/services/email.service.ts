df8dc88302267133618475e5e3dec4a1e5689f6b
durejalovish <114432571+durejalovish@users.noreply.github.com>
2025-03-18 05:29:47 +0530

    Update email.service.ts

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmailService {
-  private apiUrl = 'http://localhost:8081/api/contactUs'; // Replace with your backend API URL
+  private apiUrl = 'https://united1backend-ec1a5f2333e4.herokuapp.com/api/contactUs';  // Replace with your backend API URL
 
   constructor(private http: HttpClient) {}
 
